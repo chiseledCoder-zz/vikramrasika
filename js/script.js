544eba10344e42c8e6b0db2ed1e8ff7edfb03959
Nikhil Shirsath <niks_kool007@yahoo.com>
2016-07-29 13:55:38 +0530

    All files added
    Index.html, main.html and CSS & JS

---
+++ js/script.js
@@ -0,0 +1,44 @@
+jQuery.easing.def = "easeOutQuad";
+
+$(document).ready(function()
+{
+	var isWorkCurrentPage = true;
+	var isAboutCurrentPage = false;
+	
+	$("#logoDetailView").click(function()
+	{
+		window.location = "../../index.html";
+	});
+	
+	$("#workPage, #logo").click(function()
+		{
+			if(!isWorkCurrentPage)
+			{
+				isWorkCurrentPage = true;
+				isAboutCurrentPage = false;
+				$("#workPage").attr("class", "currentPage");
+				$("#aboutPage").removeClass("currentPage");
+				
+				$("#about").fadeOut(500, function()
+				{
+					$("#work").fadeIn(500);
+				});
+			}
+		});
+	
+	$("#aboutPage").click(function()
+		{
+			if(!isAboutCurrentPage)
+			{
+				isAboutCurrentPage = true;
+				isWorkCurrentPage = false;
+				$("#aboutPage").attr("class", "currentPage");
+				$("#workPage").removeClass("currentPage");
+				
+				$("#work").fadeOut(500, function()
+				{
+					$("#about").fadeIn(500);
+				});
+			}
+		});
+	});
